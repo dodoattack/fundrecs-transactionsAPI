3522c39818b4c1faa47447f4f904455d5d7058df
davidhowick <david.howick2@mail.dcu.ie>
2022-12-21 04:01:36 +0000

    The 'checkTransactionAreCorrectlyAddedWithoutChecks' test has been added

--- FundRecs/src/test/java/com/interview/fundrecs/model/TransactionsAggregateTest.java
+++ FundRecs/src/test/java/com/interview/fundrecs/model/TransactionsAggregateTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TransactionsAggregateTest {
     private TransactionsAggregate transactionsAggregate = new TransactionsAggregate();
@@ -36,7 +37,7 @@ public class TransactionsAggregateTest {
         transaction1.setType("credit");
         transactions.add(transaction1);
 
-        transactionsAggregate.addTransactions(transactions);
+        transactionsAggregate.addTransactionsWithChecks(transactions);
         Transaction transaction = transactionsAggregate.getTransaction("11-12-2018", "credit");
         Assert.assertEquals("Check that there are two transactions", "4.0", transaction.getAmount());
     }
@@ -51,9 +52,32 @@ public class TransactionsAggregateTest {
         transaction1.setType("debit");
         transactions.add(transaction1);
 
-        transactionsAggregate.addTransactions(transactions);
+        transactionsAggregate.addTransactionsWithChecks(transactions);
         Transaction transaction = transactionsAggregate.getTransaction("11-12-2018", "debit");
         Assert.assertEquals("Check that there are two transactions", "debit", transaction.getType());
         Assert.assertEquals("Check that there are two transactions", "3", transaction.getAmount());
+    }
+
+    @Test
+    public void checkTransactionAreCorrectlyAddedWithoutChecks(){
+        Transaction transaction1 = new Transaction();
+        transaction1.setAmount("1");
+        transaction1.setDate("11-01-2020");
+        transaction1.setType("debit");
+        Transaction transaction2 = new Transaction();
+        transaction2.setAmount("1");
+        transaction2.setDate("11-01-2020");
+        transaction2.setType("credit");
+        transactions.add(transaction1);
+        transactions.add(transaction2);
+
+        transactionsAggregate.addTransactionsWithoutChecks(transactions);
+
+        Map<String, List<Transaction>> transactions = transactionsAggregate.getTransactions();
+        List<Transaction> transactionList = transactions.get("11-01-2020");
+
+        Assert.assertEquals(2, transactionList.size());
+        Assert.assertEquals("Check that the transactions contain the same date only", "11-01-2020", transactionList.get(0).getDate());
+        Assert.assertEquals("Check that the transactions contain the same date only", "11-01-2020", transactionList.get(1).getDate());
     }
 }
