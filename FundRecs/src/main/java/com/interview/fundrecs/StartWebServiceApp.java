25566c57e3266a2046592a971b27dbf912eb984d
davidhowick <david.howick2@mail.dcu.ie>
2022-12-21 02:13:24 +0000

    Adding temporary property solution for the transactions storage path.

--- FundRecs/src/main/java/com/interview/fundrecs/StartWebServiceApp.java
+++ FundRecs/src/main/java/com/interview/fundrecs/StartWebServiceApp.java
@@ -3,9 +3,20 @@ package com.interview.fundrecs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StartWebServiceApp {
+    public static String TRANSACTIONS_STORAGE_PATH;
+
     public static void main(String [] args) {
-        SpringApplication.run(StartWebServiceApp.class, args);
+        if(args[0] == null){
+            System.out.println("Please run the app with the first input arg. being the path of the " +
+                    "transactions.json storage file");
+        } else {
+            System.out.println("The input arg found is : " + args[0]);
+
+            TRANSACTIONS_STORAGE_PATH = args[0];
+            SpringApplication.run(StartWebServiceApp.class, args);
+        }
     }
 }
