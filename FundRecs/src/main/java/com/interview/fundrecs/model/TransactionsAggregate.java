e83c55de81ec1dcb732a11120cad8baf2d13e9a8
davidhowick <david.howick2@mail.dcu.ie>
2022-12-21 03:56:46 +0000

    A 'addTransactionsWithoutChecks' function has been added.

--- FundRecs/src/main/java/com/interview/fundrecs/model/TransactionsAggregate.java
+++ FundRecs/src/main/java/com/interview/fundrecs/model/TransactionsAggregate.java
@@ -3,6 +3,7 @@ package com.interview.fundrecs.model;
 import com.interview.fundrecs.model.validations.TransactionValidation;
 import com.sun.media.sound.InvalidFormatException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 public class TransactionsAggregate {
     private Map<String, List<Transaction>> transactions = new HashMap<>();
 
-        public Map<String, List<Transaction>> addTransactions(List<Transaction> transactionRequests){
+    public void addTransactionsWithChecks(List<Transaction> transactionRequests) {
         transactionRequests.forEach(transaction -> {
             try {
                 new TransactionValidation(transaction, this.transactions)
@@ -23,11 +24,10 @@ public class TransactionsAggregate {
                 throw new RuntimeException(e.getMessage());
             }
         });
-        return this.transactions;
     }
 
-    public Transaction getTransaction(String date, String type){
-        if(this.transactions.containsKey(date)) {
+    public Transaction getTransaction(String date, String type) {
+        if (this.transactions.containsKey(date)) {
             return this.transactions
                     .get(date)
                     .stream()
@@ -37,7 +37,26 @@ public class TransactionsAggregate {
         return null;
     }
 
-    public int getTransactionsCount(){
-            return this.transactions.size();
+    public void addTransactionsWithoutChecks(List<Transaction> transactions) {
+        if (this.transactions.isEmpty()) {
+            transactions.forEach(transaction -> {
+                if(this.transactions.containsKey(transaction.getDate())) {
+                    List<Transaction> transactionList = this.transactions.get(transaction.getDate());
+                    transactionList.add(transaction);
+                } else {
+                    List<Transaction> transactionList = new ArrayList<>();
+                    transactionList.add(transaction);
+                    this.transactions.put(transaction.getDate(), transactionList);
+                }
+            });
+        }
+    }
+
+    public int getTransactionsCount() {
+        return this.transactions.size();
+    }
+
+    public Map<String, List<Transaction>> getTransactions(){
+        return this.transactions;
     }
 }
